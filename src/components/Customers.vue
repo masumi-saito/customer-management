ccf8c8c42e3574617d95a2d29dfd0fd7778224e1
masumi_saito <m.saito9696biz@gmail.com>
2020-03-05 14:21:58 +0900

    no message

---
+++ src/components/Customers.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>顧客情報管理システム</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Customers></Customers>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Customers from '@/components/Customers.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Customers
   }
 }
 </script>
